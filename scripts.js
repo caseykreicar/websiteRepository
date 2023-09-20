1feb5271c5f67de26de0e7176adf870cea1beb0d
caseykreicar <kreicarcl@alumni.vcu.edu>
2023-09-20 16:09:29 -0400

    the struggle is real with js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
+/*dark mode toggle*/
 const switchElement = document.querySelector('.switch')
 
 switchElement.addEventListener('click', () => {
   document.body.classList.toggle('dark')
 })
+
+/*fade in animation*/
+const observer = new IntersectionObserver(entries => { 
+  entries.forEach(entry => {
+    if(entry.isIntersection){
+      document.querySelectorAll("card-container")[0].classList.add("card");
+    }
+  })
+})
+
+observer.observe(document.querySelector(".card"));
