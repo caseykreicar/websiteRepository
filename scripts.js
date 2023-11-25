de67d5b23a669f90d9a57240e2c68711530b57cb
Casey Latimere Kreicar <kreicarcl@alumni.vcu.edu>
2023-11-25 18:31:39 -0500

    finished animations

--- scripts.js
+++ scripts.js
@@ -38,3 +38,20 @@ const observer2 = new IntersectionObserver(entries => {
 squirrel.forEach(squirrel => {
   observer.observe(squirrel)
 })
+
+/*fade in animation3 speech bubble*/
+const speechBubble = document.querySelectorAll(".speechBubble")
+
+const observer3 = new IntersectionObserver(entries => { 
+  entries.forEach(entry => {
+    entry.target.classList.toggle("show", entry.isIntersecting)
+    if (entry.isIntersecting) observer2.unobserve(entry.target)
+  })
+},
+{
+    threshold: .1,
+}
+)
+speechBubble.forEach(speechBubble => {
+  observer.observe(speechBubble)
+})
